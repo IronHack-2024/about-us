95269ff58b8d2bc0484d6eb19c40c8db753bf690
kevmhughes <kevhughes24@hotmail.com>
2024-11-07 01:10:31 +0100

    Format the document with Prettier.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("Matrix");
+const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
+const katakana =
+  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const nums = "0123456789";
 
 const alphabet = katakana + latin + nums;
 
@@ -21,16 +22,16 @@ for (let x = 0; x < columns; x++) {
 }
 
 const draw = () => {
-  context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
   context.fillRect(0, 0, canvas.width, canvas.height);
-  
-  context.fillStyle = '#0F0';
-  context.font = fontSize + 'px monospace';
+
+  context.fillStyle = "#0F0";
+  context.font = fontSize + "px monospace";
 
   for (let i = 0; i < rainDrops.length; i++) {
     const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
     context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
-    
+
     if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
       rainDrops[i] = 0;
     }
@@ -42,17 +43,17 @@ let opacity = 1;
 
 // Function to fade out the screen
 const fadeOut = () => {
-    if (opacity > 0) {
-      opacity -= 0.01; // Reduce opacity gradually
-      context.fillStyle = `rgba(0, 0, 0, ${1 - opacity})`;
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      requestAnimationFrame(fadeOut);
-    } else {
-      // Show the typewriter effect when fade is complete
-      document.querySelector('.typewriter-container').style.display = 'flex';
-      document.getElementById('Matrix').style.display = "none"
-    }
-  };
+  if (opacity > 0) {
+    opacity -= 0.01; // Reduce opacity gradually
+    context.fillStyle = `rgba(0, 0, 0, ${1 - opacity})`;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(fadeOut);
+  } else {
+    // Show the typewriter effect when fade is complete
+    document.querySelector(".typewriter-container").style.display = "flex";
+    document.getElementById("Matrix").style.display = "none";
+  }
+};
 
 // Start the animation
 const intervalId = setInterval(draw, 30);
